1d9c9b45ce4620bbd5e184fbd04f665a6b24ce91
shon5544 <sbs8239@gmail.com>
2023-02-26 17:55:08 +0900

    feat: added log when post created.

--- src/main/java/beomside/everybeomsu/service/PostService.java
+++ src/main/java/beomside/everybeomsu/service/PostService.java
@@ -6,6 +6,7 @@ import beomside.everybeomsu.dto.res.home.PostHomeResDto;
 import beomside.everybeomsu.repository.PostRepository;
 import beomside.everybeomsu.repository.jpa.PostJpaRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostService {
@@ -23,6 +25,7 @@ public class PostService {
     @Transactional
     public void createPost(Post post) {
         postRepository.save(post);
+        log.info("Success: the post is created successfully");
     }
 
     public Post getPostById(Long id) {
