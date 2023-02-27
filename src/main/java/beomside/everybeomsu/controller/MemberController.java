b530eb8820f0bf947f85f971bcc53da356e6e75e
shon5544 <sbs8239@gmail.com>
2023-02-27 23:55:22 +0900

    refactor: rename RegisterLoginController to MemberController

--- src/main/java/beomside/everybeomsu/controller/MemberController.java
+++ src/main/java/beomside/everybeomsu/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-public class RegisterLoginController {
+public class MemberController {
 
     private final MemberService memberService;
 
